705537639a7ce55888ebb64cf06926649e5d1a68
Bohdan Taran <bohdan.taran@inside-m2m.de>
2025-11-07 21:06:26 +0100

    feat(my-files): add endpoint, service method that extracts all user's files

--- src/main/java/com/bohdan/filesharing/file/api/FileController.java
+++ src/main/java/com/bohdan/filesharing/file/api/FileController.java
@@ -1,9 +1,11 @@
 package com.bohdan.filesharing.file.api;
 
 import com.bohdan.filesharing.file.api.dto.FileItemDto;
+import com.bohdan.filesharing.file.db.FileItem;
 import com.bohdan.filesharing.file.domain.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,11 +14,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/files")
@@ -65,5 +70,34 @@ public class FileController {
     ) {
         FileItemDto response = fileService.uploadFile(file, isPublic, auth);
         return ResponseEntity.ok(response);
+    }
+    /**
+     * Get all files by the authenticated user
+     *
+     * @return Returns all files uploaded by the current authenticated user
+     */
+    @Operation(summary = "Get my uploaded files",
+            description = "Returns all files uploaded by the current authenticated user")
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of user's files",
+            content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = FileItemDto.class))
+            )
+    )
+    @ApiResponse(
+            responseCode = "400",
+            description = "User was not found",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            value = "User with email john@example.com was not found!"
+                    )
+            ))
+    @GetMapping("/my")
+    public ResponseEntity<List<FileItem>> getMyFiles(Authentication auth) {
+        List<FileItem> myFiles = fileService.getMyFiles(auth);
+        return ResponseEntity.ok(myFiles);
     }
 }
